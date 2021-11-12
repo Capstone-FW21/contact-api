d2d2e36450579dd784de70c2da04f09b6661695c
Dylan Holmes <holmes.dylan@hotmail.com>
2021-11-12 09:29:26 -0800

    - Added version endpoint - Updated requirements to include Sarge and Poetry

--- contact-api/main.py
+++ contact-api/main.py
@@ -1,6 +1,8 @@
 import fastapi
-from fastapi import FastAPI
-from typing import Optional
+import sys
+from fastapi import FastAPI, status
+from typing import Optional, List
+from sarge import capture_stdout
 
 
 app = FastAPI()
@@ -22,9 +24,25 @@ def demo_get(demo_arg: str):
     return demo_arg
 
 
-@app.put("/demo_put/{demo_arg}/")
-def demo_put(demo_arg: str):
+@app.post("/demo_post/{demo_arg}/")
+def demo_post(demo_arg: str):
     """
     Place holder PUT request method stub.
     """
     return "OK"
+
+
+@app.get(
+    "/versions",
+    response_model=List[str],
+    tags=["versions"],
+    responses={200: {"success": status.HTTP_200_OK}},
+)
+def versions():
+    output = None
+    if sys.platform == "linux":
+        output = capture_stdout("/usr/local/bin/pip list")
+    else:
+        output = capture_stdout("poetry show --no-dev --no-ansi")
+
+    return output.stdout.text.splitlines()
